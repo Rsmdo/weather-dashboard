6f9200a79609c47cf60aa6f9d67417682e39c06d
Royden Semedo <roydensemedo@Roydens-MacBook-Pro.local>
2022-07-12 17:07:31 -0400

    started the js

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+//all needed devlarations
+var tempUnitBtn = document.getElementById("temp-unit-btn");
+var cityInputEl = document.getElementById("location");
+var searchFormEl = document.getElementById("location-form");
+var searchHistoryEl = document.querySelector(".search--history");
+var cityNameEl = document.getElementById("city-name");
+var currDateEl = document.getElementById("current-date");
+var tempEl = document.getElementById("temp");
+var windEl = document.getElementById("wind");
+var humidityEl = document.getElementById("humidity");
+var uvIndexEl = document.getElementById("uv-index");
